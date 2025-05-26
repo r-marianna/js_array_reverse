c42f86e2ca8b87f4df3cac1ee6304a1aeb007182
Marianna Repka <repka.mv@gmail.com>
2025-05-26 18:31:23 +0300

    Tests for a arrayReverse function

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,32 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse(['Mate'])).toBeInstanceOf(Array);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
+    expect(arrayReverse([''])).toEqual(['']);
+  });
+
+  it('should return the reversed string', () => {
+    expect(arrayReverse(['Hell0'])).toEqual(['0lleH']);
+  });
+
+  it('should reverse two strings', () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
-  // write more tests here
+  it('should reverse more than one string', () => {
+    expect(arrayReverse(['I', 'am', 'a', 'student!'])).toEqual([
+      '!',
+      'tn',
+      'e',
+      'dutsamaI',
+    ]);
+  });
+
+  it('should ', () => {
+    expect(arrayReverse(['I', 'stude'])).toEqual(['e', 'dutsI']);
+  });
 });
